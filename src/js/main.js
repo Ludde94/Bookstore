3c95057313ca80de6c6d1267f6d38382df41a656
ludde94 <l.jenisch@hotmail.com>
2023-03-09 17:08:42 +0100

    added pics, and buttons on display

--- src/js/main.js
+++ src/js/main.js
@@ -6,56 +6,53 @@ import * as bootstrap from 'bootstrap'
 
 import { getJSON } from './utils/getJSON';
 
-
 let persons,
-
     chosenHobbyFilter = 'all',
-
     chosenSortOption,
-
     hobbies = [];
 
-
 async function start() {
-
-    persons = await getJSON('/public/books.json');
-
+    persons = await getJSON('/books.json');
     displayPersons();
-
 }
-
-
 
 function displayPersons() {
-
     // filter according to hobby and call displayPersons
-
     let filteredPersons = persons.filter(
-
-        ({ hobby }) => chosenHobbyFilter === 'all'
-
-            || chosenHobbyFilter === hobby
-
+      ({ hobby }) =>
+        chosenHobbyFilter === 'all' || chosenHobbyFilter === hobby
     );
-
-    if (chosenSortOption === 'Last name') { sortByLastName(filteredPersons); }
-
-    if (chosenSortOption === 'Age') { sortByAge(filteredPersons); }
-
-    let htmlArray = filteredPersons.map(({
-
-        id, title, author, description, category, price
-
-    }) => /*html*/
-    
-
-  '<h1>hejsan</h1>'
   
-  )
-
+    if (chosenSortOption === 'Last name') {
+      sortByLastName(filteredPersons);
+    }
+    if (chosenSortOption === 'Age') {
+      sortByAge(filteredPersons);
+    } // WTF IS THIS?
+  
+    let htmlArray = filteredPersons.map(
+      ({ id, title, author, description, category, price, img }) =>
+        `<div class="col-lg-3 bookrow" data-id="${id}">
+           <img class="pic" src="${img}" />
+           <h6>${title}</h6>
+           <p>${author}</p>
+           <button class="btn btn-success button"><p>${price}:-</p>
+           </button>
+         </div>`
+    );
+  
     document.querySelector('.book-list').innerHTML = htmlArray.join('');
-
-}
+  
+    const buttons = document.getElementsByClassName('button');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', (event) => {
+        const bookRow = event.target.closest('.bookrow');
+        const id = bookRow.getAttribute('data-id') || bookRow.id;
+        const book = persons.find((person) => person.id === id);
+        alert(`You clicked a button with ID: ${id}`); 
+        //addToCart(id)
+      });
+    }}
 
 
 start();
