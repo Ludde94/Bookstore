61a21aa999b42fe22a42fd1244c1728bea130dee
ludde94 <l.jenisch@hotmail.com>
2023-03-09 13:05:26 +0100

    styles and nav

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,56 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-//start our app
+import { getJSON } from './utils/getJSON';
+
+
+let persons,
+
+    chosenHobbyFilter = 'all',
+
+    chosenSortOption,
+
+    hobbies = [];
+
+
+async function start() {
+
+    persons = await getJSON('/json/books.json');
+
+    displayPersons();
+
+}
+
+
+
+function displayPersons() {
+
+    // filter according to hobby and call displayPersons
+
+    let filteredPersons = persons.filter(
+
+        ({ hobby }) => chosenHobbyFilter === 'all'
+
+            || chosenHobbyFilter === hobby
+
+    );
+
+    if (chosenSortOption === 'Last name') { sortByLastName(filteredPersons); }
+
+    if (chosenSortOption === 'Age') { sortByAge(filteredPersons); }
+
+    let htmlArray = filteredPersons.map(({
+
+        id, title, author, description, category, price
+
+    }) => /*html*/
+    
+
+  'html here');
+
+    document.querySelector('.book-list').innerHTML = htmlArray.join('');
+
+}
+
+
+start();
